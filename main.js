ed29574185b10732d5024cb6cccc19986c4bbac9
Adeyi Israel <adeyiisreal3@gmail.com>
2022-07-15 20:37:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,50 +1,8 @@
-let Weather = {
-  "apikey": "ccbee0f046cde04d99db1a20a3630353",
-  fetchWeather: function(city) {
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apikey).then((Response) => Response.json())
-      .then((data) => this.displayWeather(data))
-  },
-  displayWeather: function(data) {
-const {name} = data;
-const {icon, description} = data.weather[0];
-const {temp, humidity} = data.main;
-const {speed} = data.wind;
-  document.querySelector('.city').innerText = 'weather in' + " " +name;
-  document.querySelector('.icon').src ='https://openweathermap.org/img/w' + icon + '.png';
-  document.querySelector('.description').innerText = description;
-  document.querySelector('.temp').innerText = temp + '°C';
-  document.querySelector('.humidity').innerText = 'Humidity:' +" " +humidity + '%';
-  document.querySelector('.wind').innerText = 'wind speed:' +" " +speed + "km/h";
-  document.querySelector('.weather').classList.remove('loading');
- document.body.style.backgroundImage = "url(https://source.unsplash.com/1600x900?" + name +")";
-}, 
-search: function(){
-  this.fetchWeather(document.querySelector('.search-bar').value)
-}
-};
-document.querySelector('.search button').addEventListener('click', ()=>{
-  Weather.search()
-})
-document.querySelector('.search-bar').addEventListener('keyup', (event)=>{
-  if(event.key === "Enter"){
-    Weather.search()
-  }
-})
 
-Weather.fetchWeather('Lagos');
-let Weather = {
 
-  "apikey": "ccbee0f046cde04d99db1a20a3630353",
 
-  fetchWeather: function(city) {
-
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" +
-
-        city +
-
+    
+        
         "&units=metric&appid=" +
 
         this.apikey).then((Response) => Response.json())
@@ -87,22 +45,20 @@ search: function(){
 
 }
 
-};
+};', ()=>{
 
-document.querySelector('.search button').addEventListener('click', ()=>{
 
-  Weather.search()
 
 })
 
-document.querySelector('.search-bar').addEventListener('keyup', (event)=>{
+', (event)=>{
 
-  if(event.key === "Enter"){
+  if(event.key === "){
 
-    Weather.search()
+
 
   }
 
-})
 
-Weather.fetchWeather('Lagos');
+
+
